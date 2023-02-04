616c856ae83b4c9972dd44e64bbf8bad05bdbfe1
QaYs4910 <QaYs4910@126.com>
2023-02-04 21:18:58 +0800

    passenger添加客户端服务发现

--- api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -2,8 +2,10 @@ package com.mashibing.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //nacos 客户端服务发现
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {
