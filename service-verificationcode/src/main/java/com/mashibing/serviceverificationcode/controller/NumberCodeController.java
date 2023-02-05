5508b5da04a54c75d7403ba3f156586489ec6588
QaYs4910 <QaYs4910@126.com>
2023-02-05 16:07:20 +0800

    随机生成验证码

--- service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
+++ service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
@@ -12,12 +12,20 @@ public class NumberCodeController {
     public String numberCode(@PathVariable("size") int size){
 
         System.out.println("得到的参数是:"+size);
+        //生成验证码
+        int result = (int)((Math.random()*9+ 1) * (Math.pow(10,size - 1)));
         JSONObject object = new JSONObject();
         object.put("code",1);
         object.put("message","success");
         JSONObject object1 = new JSONObject();
-        object1.put("numberCode",1234567);
+        object1.put("numberCode",result);
         object.put("data",object1);
         return object.toString();
+    }
+
+
+    public static void main(String[] args) {
+        int b = (int)((Math.random()*9+ 1) * (Math.pow(10,5)));
+        System.out.println(b);
     }
 }
