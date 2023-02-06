522b47bd6a3c7b4104cf6bb9bb229cdda1c7a8d0
QaYs4910 <QaYs4910@126.com>
2023-02-06 10:04:31 +0800

    统一返回值设置和common公共类的添加

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/dto/ResponseResult.java
@@ -0,0 +1,56 @@
+package com.mashibing.internalcommon.dto;
+
+import com.mashibing.internalcommon.constant.CommonStatusConstant;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true) //链式设置
+public class ResponseResult<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    /**
+     * 成功的响应
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> ResponseResult success( T data){
+        return new ResponseResult().setCode(CommonStatusConstant.SUCCESS.getCode()).setMessage(CommonStatusConstant.SUCCESS.getValue()).setData(data);
+    }
+
+    /**
+     * 自定义失败方法 错误码和提示
+     * @param code
+     * @param message
+     * @return
+     */
+    public static ResponseResult fail(int code,String message){
+        return new ResponseResult().setCode(code).setMessage(message);
+    }
+
+    /**
+     * 供内部服务使用接收到数据已String的方式进行传输所以data数据类型为String
+     * 自定义 失败,错误码,提示信息,具体的错误
+     * @param code
+     * @param message
+     * @param data
+     * @return
+     */
+    public static ResponseResult fail(int code,String message,String data){
+        return new ResponseResult().setCode(code).setMessage(message).setData(data);
+    }
+
+    /**
+     * 统一失败处理
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> ResponseResult fail(T data){
+        return new ResponseResult().setData(data);
+    }
+}
--- service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
+++ service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
@@ -1,6 +1,7 @@
 package com.mashibing.serviceverificationcode.controller;
 
-import net.sf.json.JSONObject;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.response.NumberCodeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,23 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberCodeController {
 
     @GetMapping("/numberCode/{size}")
-    public String numberCode(@PathVariable("size") int size){
+    public ResponseResult numberCode(@PathVariable("size") int size){
 
         System.out.println("得到的参数是:"+size);
         //生成验证码
         int result = (int)((Math.random()*9+ 1) * (Math.pow(10,size - 1)));
-        JSONObject object = new JSONObject();
-        object.put("code",1);
-        object.put("message","success");
-        JSONObject object1 = new JSONObject();
-        object1.put("numberCode",result);
-        object.put("data",object1);
-        return object.toString();
+        NumberCodeResponse numberCodeResponse = new NumberCodeResponse();
+        numberCodeResponse.setNumberCode(result);
+        return ResponseResult.success(numberCodeResponse);
     }
 
-
-    public static void main(String[] args) {
-        int b = (int)((Math.random()*9+ 1) * (Math.pow(10,5)));
-        System.out.println(b);
-    }
 }
