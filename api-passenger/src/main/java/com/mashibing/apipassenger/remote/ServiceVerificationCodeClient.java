bfe0041ccba12da3b0e51e9e68a2e30bd3ff661d
QaYs4910 <QaYs4910@126.com>
2023-02-06 12:04:33 +0800

    remote 客户端接口修改numberCode固定验证码的长度设置

--- api-passenger/src/main/java/com/mashibing/apipassenger/remote/ServiceVerificationCodeClient.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/remote/ServiceVerificationCodeClient.java
@@ -3,12 +3,13 @@ package com.mashibing.apipassenger.remote;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("service-verificationcode")
 public interface ServiceVerificationCodeClient {
 
-    @RequestMapping(method = RequestMethod.GET,value = "/numberCode/6")
-    ResponseResult<NumberCodeResponse> numberCode();
+    @RequestMapping(method = RequestMethod.GET,value = "/numberCode/{size}")
+    ResponseResult<NumberCodeResponse> numberCode(@PathVariable("size") int size);
 }
--- api-passenger/src/main/java/com/mashibing/apipassenger/service/imp/VerificationCodeServiceImp.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/imp/VerificationCodeServiceImp.java
@@ -11,16 +11,20 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class VerificationCodeServiceImp implements VerificationCodeService {
-
+    //验证码的生成位数
+    private static final int NUMBER_CODE_SIZE = 6;
+    //remote client interface
     private final ServiceVerificationCodeClient serviceVerificationCodeClient;
+
     @Override
     public String generatorCode(String passengerPhone) {
+
         /**
          * 1.调用远程验证码服务,获取验证码
          * 2.存如redis
          */
         System.out.println("调用验证码服务,获取验证码");
-        ResponseResult<NumberCodeResponse> numberCodeResponse = this.serviceVerificationCodeClient.numberCode();
+        ResponseResult<NumberCodeResponse> numberCodeResponse = this.serviceVerificationCodeClient.numberCode(NUMBER_CODE_SIZE);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         System.out.println("remote number code:"+numberCode);
